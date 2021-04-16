2b078a865668b3781b08ae3bb5421ff55aae85ee
thorsten <thorstenhempel@web.de>
2021-04-16 19:01:41 +0200

    Update cythonrecipe

--- contrib/p4a/deeponion-x13-hash/__init__.py
+++ contrib/p4a/deeponion-x13-hash/__init__.py
@@ -3,11 +3,11 @@ from pythonforandroid.recipe import CythonRecipe
 
 class X13HashRecipe(CythonRecipe):
 
-    url = ('https://files.pythonhosted.org/packages/'
-           'source/x/deeponion-x13-hash/deeponion-x13-hash-{version}.tar.gz')
+    url = ('https://files.pythonhosted.org/packages/78/d1/dfd9b0f2b095787ce6da6876199e71681773cfab22321e63174e05919b6c/deeponion-x13-hash-{version}.tar.gz')
     md5sum = '08c55917bc73a3552d332c1bd11e0b0a'
     version = '1.0.4'
-    depends = ['python3']
+    depends = ['python3', 'setuptools']
+    call_hostpython_via_targetpython = False
 
 
 recipe = X13HashRecipe()
