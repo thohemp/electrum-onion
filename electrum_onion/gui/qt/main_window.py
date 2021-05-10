5b35bc6f4dce39154abf598cfd59515f6478486b
thorsten <thorstenhempel@web.de>
2021-05-10 09:47:57 +0200

    Disable lightning

--- electrum_onion/gui/qt/main_window.py
+++ electrum_onion/gui/qt/main_window.py
@@ -729,7 +729,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
         view_menu = menubar.addMenu(_("&View"))
         add_toggle_action(view_menu, self.addresses_tab)
         add_toggle_action(view_menu, self.utxo_tab)
-        add_toggle_action(view_menu, self.channels_tab)
+        # add_toggle_action(view_menu, self.channels_tab)
         add_toggle_action(view_menu, self.contacts_tab)
         add_toggle_action(view_menu, self.console_tab)
 
